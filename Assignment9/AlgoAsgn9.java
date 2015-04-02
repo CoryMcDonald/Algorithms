921525eb8154dddaa711115d9208fd5cda0394df
Cory McDonald <Cory@Corywmcdonald.com>
2015-04-01 19:54:15 -0500

    removed unneeded prints

--- Assignment9/AlgoAsgn9.java
+++ Assignment9/AlgoAsgn9.java
@@ -10,17 +10,12 @@ class AlgoAsgn9
         {
             if(prevVertex != -1)
             {
-                // System.out.print(length + "+"+ adjacency_matrix[prevVertex-1][p-1] + " = ");
                 length += adjacency_matrix[prevVertex-1][p-1];
-                // System.out.print(length);
-                // System.out.print("\t" + p + ":" + adjacency_matrix[prevVertex-1][p-1]);
             }else if(prevVertex == -1)
             {
-                // System.out.print(p + " ");
             }
             prevVertex = p;
         }
-        // System.out.print("= " + length + "\n ");
         return length;
         // return u.parents.size();
     }
@@ -29,13 +24,7 @@ class AlgoAsgn9
         //so we need to go through column row that isn't in parents
         //so
         int bound = length(u, adjacency_matrix);
-        // System.out.print("Length: " + bound + "; Parents: ");
-        // for(int p : u.parents)
-        //     System.out.print(p + " ");
-
-        // if(u.parents.size() > 1 )
-        //     System.out.println(u.parents.get(u.parents.size()-1));
-        // System.out.println("2,2: " + adjacency_matrix[1][4]);
+        
         for(int i=0; i<adjacency_matrix[0].length; i++) //each row
         {
             boolean addFlag = true;
@@ -54,23 +43,19 @@ class AlgoAsgn9
                         if(u.parents.size() >= 1 && i+1 == u.parents.get(u.parents.size()-1) && !u.parents.contains(j+1))
                         {
                             minForRow = adjacency_matrix[i][j];                         
-                            // System.out.print(adjacency_matrix[i][j] +" ");
                         }
                         else if((!u.parents.contains(j+1) || j==0) && (i+1 != u.parents.get(u.parents.size()-1)))
                         {
                             minForRow = adjacency_matrix[i][j];                         
-                            // System.out.print(adjacency_matrix[i][j] +", ");
                         }
                     }
                 }
                 if(minForRow != Integer.MAX_VALUE)
                 {
                     bound += minForRow;
-                    // System.out.print(" = min: " + minForRow + "\n");
                 }
             }
         }
-        // System.out.println("Bound: " + bound);
         return bound;
     }
     static void Travel(int adjacency_matrix[][])
@@ -82,11 +67,10 @@ class AlgoAsgn9
         int minlength = Integer.MAX_VALUE;
         Point v = new Point();
         v.level = 0;
-        v.parents.add(1); //path so far
+        v.parents.add(1); 
         v.bound = bound(v, adjacency_matrix);
 
         queue.add(v);
-        //todo insert the vertex into it
         while(queue.size() > 0) {
             Point current = queue.remove();
             if(current.bound < minlength)
@@ -95,14 +79,12 @@ class AlgoAsgn9
                 {
                     if(!current.parents.contains(i))
                     {
-                        // System.out.println("i: " + i);
                         Point u = new Point(current.parents);
                         u.level = current.level +1;
                         u.parents.add(i);
                         u.bound = v.bound;
                         // for(int p : u.parents)
                         //     System.out.print(p + " ");
-                        // System.out.print("\n");
                         if(u.level == n-2)
                         {
                             //put inde of only vertex remaing at the end of u.parents
@@ -117,31 +99,21 @@ class AlgoAsgn9
                                 minlength = length(u, adjacency_matrix);
                                 minPoint = u;
                                 opttour = u.parents;
-                                // System.out.println("\tcircuit found: " + minlength);
                             }
                         }else
                         {
                             u.bound = bound(u, adjacency_matrix);
                             if(u.bound < minlength)
                             {
-                                // System.out.println("\tAdding to queue, bound = " + u.bound);
                                 queue.add(u);
-                            }else 
-                            {
-                                // System.out.println("\tNot adding : " + i + " becaues bound = " + u.bound);
                             }
                         }
                     }
                 }
-            }else
-            {
-                // System.out.println("Bound > min: " + current.bound);
             }
         }
         if(minPoint !=null)
-        System.out.println(length(minPoint, adjacency_matrix));
-        // int length = adjacency_matrix[opttour.get(opttour.size())][1];
-        // System.out.print("1" + length(opttour, adjacency_matrix) + "\n");
+            System.out.println(length(minPoint, adjacency_matrix));
     }
 
     public static void main(String... arg)
@@ -173,42 +145,10 @@ class AlgoAsgn9
             }
             if(count != 1 && (input.equals("") || !scanner.hasNext()))
             {
-                // for (int i = 0; i <= adjacency_matrix[0].length-1; i++)
-                // {
-                //     for (int j = 0; j <= adjacency_matrix[0].length-1; j++)
-                //     {
-                //         if (adjacency_matrix[i][j] == 1 && adjacency_matrix[j][i] == 0)
-                //         {
-                //             adjacency_matrix[j][i] = 1;
-                //         }
-                //     }
-                // }
                 Travel(adjacency_matrix);
             }
             count++;
         }
-        // System.out.println(scanner.hasNext());
-        // for (int i = 1; i <= adjacency_matrix[0].length-1; i++)
-        // {
-        //     for (int j = 1; j <= adjacency_matrix[0].length-1; j++)
-        //     {
-        //         System.out.print(adjacency_matrix[i][j] + " ");
-        //     }
-        //     System.out.print("\n");
-        // }
-
-        // for (int i = 1; i <= 4; i++)
-        // {
-        //     for (int j = 1; j <= 4; j++)
-        //     {
-        //         if (adjacency_matrix[i][j] == 1 && adjacency_matrix[j][i] == 0)
-        //         {
-        //             adjacency_matrix[j][i] = 1;
-
-        //         }
-        //     }
-        // }
-    
         scanner.close();
     }
 }
